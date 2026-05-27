3064f6ce66cd9109637c7eb4c1262580ef1a6aa5
Doojin Jang <takako4u@gmail.com>
2026-05-27 15:42:05 +0900

    Update noise_analysis_basic.py

--- noise_analysis_basic.py
+++ noise_analysis_basic.py
@@ -18,8 +18,8 @@ tab_select = st.sidebar.radio(
     ["1. Stochastic & PDF", "2. PSD & kT/C Noise", "3. Correlated Sources"]
 )
 
-    st.sidebar.markdown("---")
-    st.sidebar.caption("Designed by Doojin Jang © 2026 ORBIT LAB. All Rights Reserved.")
+st.sidebar.markdown("---")
+st.sidebar.caption("Designed by Doojin Jang © 2026 ORBIT LAB. All Rights Reserved.")
 # ----------------------------------------------------------------
 # Tab 1: Stochastic Process & PDF
 # ----------------------------------------------------------------
